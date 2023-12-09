9b59d84e4770151f6150d885280e517d3bce845b
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-12-09 16:56:26 +0500

    Changes made

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const score_0 = document.querySelector('#score--0');
+const score_1 = document.querySelector('#score--1');
+const current_0 = document.querySelector('#current--0');
+const current_1 = document.querySelector('#current--1');
+
+const dice_image = document.querySelector('.dice');
+const new_game_btn = document.querySelector('.btn--new');
+const roll_dice_btn = document.querySelector('.btn--roll');
+const hold_btn = document.querySelector('.btn--hold');
+
+
